96917dc77b98e13d811fd32c502472cecf93b3c7
Djacon <danielille100@gmail.com>
2023-09-01 11:41:36 +0300

    Add BotMedium, Fix Attack/Upgrade handlers

--- python/main.py
+++ python/main.py
@@ -1,22 +1,21 @@
 from fastapi import FastAPI, HTTPException
+# from time import time
 
 from game import Game
 
 app = FastAPI()
+game = Game()
 
 
 @app.post('/ai_attack')
 async def receive_json(data: dict):
-    game = Game(data)
+    game.reinit(data)
     game.doTurn()
     return game.actions
-    # except Exception as e:
-    #     print(e)
-    #     raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post('/ai_upgrade')
 async def receive_json(data: dict):
-    game = Game(data)
+    game.reinit(data)
     game.doUpgrade()
     return game.actions
